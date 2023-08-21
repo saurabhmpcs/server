87af8efce746b512f8740a9668944ee463a06b06
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-21 12:00:23 +0530

    intial-commit

---
+++ index.js
@@ -0,0 +1,25 @@
+import express from "express";
+import mongoose, { Mongoose } from "mongoose";
+import cors from "cors";
+
+const app = express();
+app.use(express.json({ limit: "30mb", extended: true }));
+app.use(express.urlencoded({ limit: "30mb", extended: true }));
+app.use(cors());
+
+app.get("/", (req, res) => {
+  res.send("This is a stack overflow clone API");
+});
+
+const PORT = process.env.PORT || 5000;
+const CONNECTION_URL =
+  "mongodb+srv://admin:admin@stack-overflow-clone.qgwyim0.mongodb.net/?retryWrites=true&w=majority";
+
+mongoose
+  .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopoLogy: true })
+  .then(() =>
+    app.listen(PORT, () => {
+      console.log(`server running on port ${PORT}`);
+    })
+  )
+  .catch((err) => console.log(err.message));
