3ae6afa5a8f12f2015daea82d0a67a92e27d5a0f
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-21 12:43:37 +0530

    signup and login auth

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from "express";
 import mongoose, { Mongoose } from "mongoose";
 import cors from "cors";
 
+import userRoutes from "./routes/users.js";
+
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
 app.use(express.urlencoded({ limit: "30mb", extended: true }));
@@ -10,6 +12,8 @@ app.use(cors());
 app.get("/", (req, res) => {
   res.send("This is a stack overflow clone API");
 });
+
+app.use("/user", userRoutes);
 
 const PORT = process.env.PORT || 5000;
 const CONNECTION_URL =
