087efa31a86e63c3a2c786af7d96342eaa6e88dd
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-23 18:28:49 +0530

    SignUP authentication

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 import express from "express";
 import mongoose from "mongoose";
 import cors from "cors";
-
 import userRoutes from "./routes/users.js";
+
+import dotenv from "dotenv";
+dotenv.config();
 
 const app = express();
 app.use(express.json({ limit: "30mb", extended: true }));
@@ -10,14 +12,15 @@ app.use(express.urlencoded({ limit: "30mb", extended: true }));
 app.use(cors());
 
 app.get("/", (req, res) => {
-  res.send("This is a stack overflow clone API");
+  res.send("This is stackoverflow clone api");
 });
 
 app.use("/user", userRoutes);
 
-const PORT = process.env.PORT || 5000;
+const PORT = 5000;
+
 const CONNECTION_URL =
-  "mongodb+srv://admin:admin@cluster0.98zf3zx.mongodb.net/?retryWrites=true&w=majority";
+  "mongodb+srv://admin:admin@stack-overflow-clone.g5h4jv2.mongodb.net/?retryWrites=true&w=majority";
 
 mongoose
   .connect(CONNECTION_URL, { useNewUrlParser: true, useUnifiedTopology: true })
--- models/auth.js
+++ models/auth.js
@@ -4,8 +4,8 @@ const userSchema = mongoose.Schema({
   name: { type: String, required: true },
   email: { type: String, required: true },
   password: { type: String, required: true },
+  about: { type: String },
   tags: { type: [String] },
-  about: { type: [String] },
   joinedOn: { type: Date, default: Date.now },
 });
 
