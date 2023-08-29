f6490225dbb35d617a14cf86bd44949953dc12cb
Saurabh Singh <saurabh.rgt@gmail.com>
2023-08-29 11:51:51 +0530

    delete-question

--- controllers/Answers.js
+++ controllers/Answers.js
@@ -30,24 +30,24 @@ const updateNoOfQuestions = async (_id, noOfAnswers) => {
   }
 };
 
-// export const deleteAnswer = async (req, res) => {
-//   const { id: _id } = req.params;
-//   const { answerId, noOfAnswers } = req.body;
+export const deleteAnswer = async (req, res) => {
+  const { id: _id } = req.params;
+  const { answerId, noOfAnswers } = req.body;
 
-//   if (!mongoose.Types.ObjectId.isValid(_id)) {
-//     return res.status(404).send("Question unavailable...");
-//   }
-//   if (!mongoose.Types.ObjectId.isValid(answerId)) {
-//     return res.status(404).send("Answer unavailable...");
-//   }
-//   updateNoOfQuestions(_id, noOfAnswers);
-//   try {
-//     await Questions.updateOne(
-//       { _id },
-//       { $pull: { answer: { _id: answerId } } }
-//     );
-//     res.status(200).json({ message: "Successfully deleted..." });
-//   } catch (error) {
-//     res.status(405).json(error);
-//   }
-// };
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("Question unavailable...");
+  }
+  if (!mongoose.Types.ObjectId.isValid(answerId)) {
+    return res.status(404).send("Answer unavailable...");
+  }
+  updateNoOfQuestions(_id, noOfAnswers);
+  try {
+    await Questions.updateOne(
+      { _id },
+      { $pull: { answer: { _id: answerId } } }
+    );
+    res.status(200).json({ message: "Successfully deleted..." });
+  } catch (error) {
+    res.status(405).json(error);
+  }
+};
--- controllers/Questions.js
+++ controllers/Questions.js
@@ -16,9 +16,70 @@ export const AskQuestion = async (req, res) => {
 
 export const getAllQuestions = async (req, res) => {
   try {
-    const questionList = await Questions.find();
+    const questionList = await Questions.find().sort({ askedOn: -1 });
     res.status(200).json(questionList);
   } catch (error) {
     res.status(404).json({ message: error.message });
+  }
+};
+
+export const deleteQuestion = async (req, res) => {
+  const { id: _id } = req.params;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("question unavailable...");
+  }
+
+  try {
+    await Questions.findByIdAndRemove(_id);
+    res.status(200).json({ message: "successfully deleted..." });
+  } catch (error) {
+    res.status(404).json({ message: error.message });
+  }
+};
+
+export const voteQuestion = async (req, res) => {
+  const { id: _id } = req.params;
+  const { value } = req.body;
+  const userId = req.userId;
+
+  if (!mongoose.Types.ObjectId.isValid(_id)) {
+    return res.status(404).send("question unavailable...");
+  }
+
+  try {
+    const question = await Questions.findById(_id);
+    const upIndex = question.upVote.findIndex((id) => id === String(userId));
+    const downIndex = question.downVote.findIndex(
+      (id) => id === String(userId)
+    );
+
+    if (value === "upVote") {
+      if (downIndex !== -1) {
+        question.downVote = question.downVote.filter(
+          (id) => id !== String(userId)
+        );
+      }
+      if (upIndex === -1) {
+        question.upVote.push(userId);
+      } else {
+        question.upVote = question.upVote.filter((id) => id !== String(userId));
+      }
+    } else if (value === "downVote") {
+      if (upIndex !== -1) {
+        question.upVote = question.upVote.filter((id) => id !== String(userId));
+      }
+      if (downIndex === -1) {
+        question.downVote.push(userId);
+      } else {
+        question.downVote = question.downVote.filter(
+          (id) => id !== String(userId)
+        );
+      }
+    }
+    await Questions.findByIdAndUpdate(_id, question);
+    res.status(200).json({ message: "voted successfully..." });
+  } catch (error) {
+    res.status(404).json({ message: "id not found" });
   }
 };
